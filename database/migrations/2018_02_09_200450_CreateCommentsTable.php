d6e2c226dc02210d6cbe7c2c2992ae9821a383b8
Islam Dudaev <islamwolf23@gmail.com>
2018-02-09 20:13:18 +0000

    Comments table finished

---
+++ database/migrations/2018_02_09_200450_CreateCommentsTable.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('text');
+
+            // data of anonymous user: name, email and site
+            $table->string('name');
+            $table->string('email');
+            $table->string('site')->nullable();
+
+            // id of the parent comment - replied to
+            $table->integer('parent_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
