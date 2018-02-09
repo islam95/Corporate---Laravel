604d9adabe6211a35b2c5f3b602c3513411871a7
Islam Dudaev <islamwolf23@gmail.com>
2018-02-09 21:01:13 +0000

    Changed articles table
    Linked articles table with users and
    categories table via migration.

---
+++ database/migrations/2018_02_09_205128_ChangeArticlesTable.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            // linking articles and users tables.
+            $table->integer('user_id')->unsigned()->default(1); // unsigned - only positive integer.
+            $table->foreign('user_id')->references('id')->on('users');
+            // linking articles and categories tables.
+            $table->integer('category_id')->unsigned()->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
