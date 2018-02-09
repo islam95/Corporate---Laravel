b887e3489bfacc519a82d0e2b4942cf89cea5a35
Islam Dudaev <islamwolf23@gmail.com>
2018-02-09 19:58:10 +0000

    Tags table finished

--- database/migrations/2018_02_09_195525_CreateTagsTable.php
+++ database/migrations/2018_02_09_195525_CreateTagsTable.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePortfoliosTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,10 @@ class CreatePortfoliosTable extends Migration
      */
     public function up()
     {
-        Schema::create('portfolios', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title', 255);
-            $table->text('text');
-            $table->string('customer', 150);
             $table->string('alias', 150)->unique(); // used in url instead of id
-            $table->string('img');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreatePortfoliosTable extends Migration
      */
     public function down()
     {
-        Schema::drop('portfolios');
+        Schema::drop('tags');
     }
 }
