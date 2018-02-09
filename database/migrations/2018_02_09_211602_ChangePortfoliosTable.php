c0db20586a35dcc8a1714a54d0a12f9e9453cbfe
Islam Dudaev <islamwolf23@gmail.com>
2018-02-09 21:27:06 +0000

    Portfolios table changed
    Linked to tags table using alias

---
+++ database/migrations/2018_02_09_211602_ChangePortfoliosTable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('portfolios', function (Blueprint $table) {
+          // linking portfolios with tags table using alias in tags.
+          $table->string('tag_alias');
+          $table->foreign('tag_alias')->references('alias')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('portfolios', function (Blueprint $table) {
+            //
+        });
+    }
+}
