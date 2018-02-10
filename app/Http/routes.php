7aa6ac1f016a5ab443d15e826d496152ab752843
Islam Dudaev <islamwolf23@gmail.com>
2018-02-10 14:46:45 +0000

    Set route for home page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+// Making resource controller for home page of the website and renaming the route from index to home.
+Route::resource('/', 'IndexController', ['only' => ['index'], 'names' => ['index'=>'home']]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::auth();
 
-Route::get('/home', 'HomeController@index');
+
