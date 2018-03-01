a8730b9623af8235df31fcca24f13cbe7833b455
Islam Dudaev <islamwolf23@gmail.com>
2018-03-01 20:18:35 +0000

    Display portolio content

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,11 +2,34 @@
 
 namespace Corp\Http\Controllers;
 
+use Corp\Models\Menu;
+use Corp\Repositories\MenuRepository;
+use Corp\Repositories\PortfolioRepository;
 use Illuminate\Http\Request;
 
 use Corp\Http\Requests;
 
 class PortfolioController extends SiteController
 {
-    //
+    public function __construct(PortfolioRepository $portfolios){
+        parent::__construct(new MenuRepository(new Menu()));
+        $this->portfolios = $portfolios;
+        $this->template = env('THEME') . '.portfolio';
+    }
+
+    /**
+     * @return $this
+     * @throws \Throwable
+     */
+    public function index(){
+        // ------------------- Content of the main Portfolio page --------------------
+        $content = view(env('THEME') .'.portfolio.main')->render();
+        $this->vars = array_add($this->vars, 'content', $content);
+
+        $this->title = 'Portfolio';
+
+        return $this->renderOutput();
+    }
+
+
 }
