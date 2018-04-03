6aee1daea7b05f366d01e43cf36b9b239c275298
Islam Dudaev <islamwolf23@gmail.com>
2018-04-03 12:14:17 +0100

    Install Image Intervention dependency

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
 
         Lavary\Menu\ServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -209,6 +210,7 @@ return [
         'Menu'      => Lavary\Menu\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
